720daaee32bcf4c16499901283bd4fe150552102
Suyoung Yun <87157566+Suyoung225@users.noreply.github.com>
2023-08-30 07:12:21 +0900

    Update 절댓값 힙_11286.java

--- BaekJoon/DataStructure/절댓값 힙_11286.java
+++ BaekJoon/DataStructure/절댓값 힙_11286.java
@@ -1,37 +1,34 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
+public class Main {
 
-	public static void main(String[] args) throws IOException {
-		// 입력
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
 
-		// 조건에 맞게 우선순위 큐 정렬 조건 변경
-		PriorityQueue<Integer> pq = new PriorityQueue<>((x, y) ->{
-			if(Math.abs(x) != Math.abs(y)){
-				return Math.abs(x) - Math.abs(y);
-			}else{
-				return x - y;
-			}
-		});
+        // 절댓값의 오름차순으로 정렬되는 우선순위 큐(pq) 생성
+        PriorityQueue<Integer> pq = new PriorityQueue<>((x, y) -> {
+            if (Math.abs(x) != Math.abs(y)) {
+                return Math.abs(x) - Math.abs(y); // 절댓값이 다르면 절댓값으로 비교
+            } else {
+                return x - y; // 절댓값이 같으면 원래 값으로 비교
+            }
+        });
 
-		// 입력 및 연산
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			int num = Integer.parseInt(st.nextToken());
-			if(num == 0){
-				if(pq.isEmpty()) System.out.println("0");
-				else System.out.println(pq.poll());
-			}else{
-				pq.add(num);
-			}
-		}
-
-	}
-
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int num = Integer.parseInt(st.nextToken());
+            if (num == 0) {
+                if (pq.isEmpty()) {
+                    System.out.println("0"); // 큐가 비어있을 때
+                } else {
+                    System.out.println(pq.poll()); // 최솟값 출력 및 제거
+                }
+            } else {
+                pq.add(num); // 0이 아닌 수를 큐에 추가
+            }
+        }
+    }
 }
-
-
